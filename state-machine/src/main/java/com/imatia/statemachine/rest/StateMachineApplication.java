5ca658c6ae5b0d1d0a4870798cbd9cc8b00755fe
daniel <leinadfonfria@gmail.com>
2021-08-12 13:21:19 +0200

    not yet

--- state-machine/src/main/java/com/imatia/statemachine/rest/StateMachineApplication.java
+++ state-machine/src/main/java/com/imatia/statemachine/rest/StateMachineApplication.java
@@ -1,4 +1,4 @@
-package com.imatia.statemachine.config;
+package com.imatia.statemachine.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
